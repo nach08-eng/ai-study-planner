3fc24e313081aec6b7e81f5afa9fb6d5ca073de7
Nachammai S <nachammaisubbu2006@gmail.com>
2026-03-28 23:22:15 +0530

    fix server PORT issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const { createApp } = require("./app");
 
 const app = createApp();
-const PORT = process.env.PORT || https://ai-study-planner-bgvf.onrender.com;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`AI Career Guidance API running on port ${PORT}`);
